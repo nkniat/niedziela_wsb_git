c18e7794222864b426007516afc18044a6c5db19
nkniat <101292925+nkniat@users.noreply.github.com>
2023-09-10 10:17:47 +0200

    Update funkcja_ryzyko.py

--- funkcja_ryzyko.py
+++ funkcja_ryzyko.py
@@ -1,4 +1,7 @@
 #  funkcja poziom_ryzyka zwraca liczbę od 0 do 10, która okresla poziom ryzyka,
 #  gdzie 0 - brak ryzyka; 10 - ekstremalnie wysokie ryzyko
 def poziom_ryzyka(wiek, plec, wyksztalcenie):
-    pass
+    if wiek < 18:
+        return 0
+    else:
+        return 1
